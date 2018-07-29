6d9b3af67f336550656cb03cba56235aeec849bd
Thomas Holmes <shadox@gmail.com>
2018-07-28 20:54:19 -0400

    use PNG functions

--- cmd/imgret/main.go
+++ cmd/imgret/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"image/png"
 	"io"
 	"io/ioutil"
 	"log"
@@ -11,7 +11,18 @@ import (
 )
 
 func main() {
-	file, err := createImage()
+	tempDir, err := ioutil.TempDir("", "image")
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	pic, err := os.Open("purple.png")
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer pic.Close()
+
+	file, err := createImage(tempDir, pic)
 	if err != nil {
 		log.Panicln(err)
 	}
@@ -25,33 +36,58 @@ func main() {
 	if err != nil {
 		log.Panicln(err)
 	}
-}
 
-func createImage() (*os.File, error) {
-	fmt.Println("Hello world")
-
-	dir, err := ioutil.TempDir("", "img")
+	out, err := os.Create(filepath.Join(tempDir, "picture2.png"))
 	if err != nil {
-		return nil, err
+		log.Panicln(err)
+	}
+	defer out.Close()
+
+	if _, err = pic.Seek(0, 0); err != nil {
+		log.Panicln("seek", err)
 	}
 
+	err = useImageFuncs(pic, out)
+	if err != nil {
+		log.Panicln("useImageFuncs", err)
+	}
+
+	out.Close()
+	cmd2 := exec.Command("xdg-open", out.Name())
+	err = cmd2.Run()
+	if err != nil {
+		log.Panicln("cmd2", err)
+	}
+
+}
+
+func createImage(dir string, img io.Reader) (*os.File, error) {
 	file, err := os.Create(filepath.Join(dir, "picture.png"))
 	if err != nil {
 		return nil, err
 	}
 	log.Println("New file", file.Name())
 
-	pic, err := os.Open("purple.png")
-	if err != nil {
-		return nil, err
-	}
-	defer pic.Close()
-
-	io.Copy(file, pic)
+	io.Copy(file, img)
 
 	_, err = file.Seek(0, 0)
 	if err != nil {
+		defer file.Close()
 		return nil, err
 	}
 	return file, nil
+}
+
+func useImageFuncs(imgData io.Reader, imgTarget io.Writer) error {
+	img, err := png.Decode(imgData)
+	if err != nil {
+		return err
+	}
+
+	err = png.Encode(imgTarget, img)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
