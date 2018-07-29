42bd77bfe40c573752c197469a1b5164d2422f62
Thomas Holmes <shadox@gmail.com>
2018-07-28 20:33:29 -0400

    Copy a picture and open it

--- cmd/imgret/main.go
+++ cmd/imgret/main.go
@@ -6,31 +6,52 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 )
 
 func main() {
+	file, err := createImage()
+	if err != nil {
+		log.Panicln(err)
+	}
+	err = file.Close()
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	cmd := exec.Command("xdg-open", file.Name())
+	err = cmd.Run()
+	if err != nil {
+		log.Panicln(err)
+	}
+}
+
+func createImage() (*os.File, error) {
 	fmt.Println("Hello world")
 
 	dir, err := ioutil.TempDir("", "img")
 	if err != nil {
-		log.Panicln(err)
+		return nil, err
 	}
 
 	file, err := os.Create(filepath.Join(dir, "picture.png"))
 	if err != nil {
-		log.Panicln(err)
+		return nil, err
 	}
-	defer file.Close()
-
 	log.Println("New file", file.Name())
 
 	pic, err := os.Open("purple.png")
 	if err != nil {
-		log.Panicln(err)
+		return nil, err
 	}
 	defer pic.Close()
 
-	log.Println("Copying")
 	io.Copy(file, pic)
+
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
 }
